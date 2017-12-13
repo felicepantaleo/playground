a8a1d07d2652bc09de8f244eaccdcad97a241ea9
fpantale <felice.pantaleo@cern.ch>
2017-12-13 14:55:05 +0100

    fixing second unit test

--- python/pqueue.py
+++ python/pqueue.py
@@ -1,25 +1,40 @@
-class myqueue:
-
-    def __init__(self):
-        self._size = 0
-        self._container = []
-
-    def push(self, elt):
-        self._size +=1
-        self._container.append(elt)
-
-    def pop(self):
-        if self._size > 0:
-            self._size-=1
-            return self._container.pop(0)
-        else:
-            raise EmptyQueueError("empty queue!")
 
 
-class EmergencyQueue(myqueue):
-    def push_front(self,elt):
-        self._container.insert(0,elt)
-
-
-class EmptyQueueError(Exception):
+class EmptyQueue():
     pass
+
+class PriorityQueue():
+    def add(self):
+        pass
+    def pop(self):
+        pass
+
+    pass
+
+
+#
+# class myqueue:
+#
+#     def __init__(self):
+#         self._size = 0
+#         self._container = []
+#
+#     def push(self, elt):
+#         self._size +=1
+#         self._container.append(elt)
+#
+#     def pop(self):
+#         if self._size > 0:
+#             self._size-=1
+#             return self._container.pop(0)
+#         else:
+#             raise EmptyQueueError("empty queue!")
+#
+#
+# class EmergencyQueue(myqueue):
+#     def push_front(self,elt):
+#         self._container.insert(0,elt)
+#
+#
+# class EmptyQueueError(Exception):
+#     pass
