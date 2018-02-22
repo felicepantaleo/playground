a226dc2202ec319a85ea4800d4e4d291f5b03ba9
Felice Pantaleo <felixpantaleo@gmail.com>
2018-02-22 16:34:37 +0100

    inizialization bonker in cpp

---
+++ cpp/initialization_bonker.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+struct foo {
+    foo() = default;
+    int a;
+};
+
+struct bar {
+    bar();
+    int b;
+};
+
+bar::bar() = default;
+
+int main() {
+    foo a{};
+    bar b{};
+    std::cout << a.a << ' ' << b.b;
+}
