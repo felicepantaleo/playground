e70ae88c06bc73d0f7af8c80c487a86074db5973
Felice Pantaleo <felixpantaleo@gmail.com>
2018-02-28 18:10:46 +0100

    checking distributions

--- clustering/ticl/main.cc
+++ clustering/ticl/main.cc
@@ -2,6 +2,8 @@
 #include <atomic>
 #include <chrono>
 #include <iostream>
+#include <iomanip>
+
 #include <sstream>
 #include <stdlib.h>
 #include <string.h>
@@ -9,61 +11,31 @@
 #include <thread>
 #include <unistd.h>
 
-static void show_usage(std::string name) {
-  std::cerr << "\nUsage: " << name << " <option(s)>"
-            << " Options:\n"
-            << "\t-h,--help\t\tShow this help message\n"
-            << "\t-n <number of points>\tSpecify the number of points to use "
-               "for the kdtree [default 100000]\n"
-            << "\t-j <number of threads>\tSpecify the number of tbb parallel "
-               "threads to use [default 1]\n"
-            << std::endl;
-}
+
+
+#include "dist_generator.h"
+#include "hgcPoint.h"
+
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    show_usage(argv[0]);
-    return 1;
-  }
 
-  int nPoints = 100000;
+  int nPoints = 1000;
   unsigned int numberOfThreads = 1;
-  for (int i = 1; i < argc; ++i) {
-    std::string arg = argv[i];
-    if ((arg == "-h") || (arg == "--help")) {
-      show_usage(argv[0]);
-      return 0;
-    }
 
-    else if (arg == "-n") {
-      if (i + 1 < argc) // Make sure we aren't at the end of argv!
-      {
-        i++;
-        std::istringstream ss(argv[i]);
-        {
-          if (!(ss >> nPoints))
-            std::cerr << "Invalid number " << argv[i] << '\n';
-          exit(1);
-        }
-      }
-    }
 
-    else if (arg == "-j") {
-      if (i + 1 < argc) // Make sure we aren't at the end of argv!
-      {
-        i++;
-        std::istringstream ss(argv[i]);
-        if (!(ss >> numberOfThreads)) {
-          std::cerr << "Invalid number of threads " << argv[i] << '\n';
-
-          exit(1);
-        }
-      }
-    }
-  }
   tbb::task_scheduler_init init(numberOfThreads);
 
+  auto avgX = -10.f;
+  auto avgY = 2.f;
+  auto peakE = 2.f;
 
+  auto sigmaXY = 2.f;
 
+  std::vector<hgcPoint> points_cloud =  generate_cluster(avgX, avgY,  peakE, sigmaXY, nPoints);
+
+  for (auto& p : points_cloud)
+  {
+      std::cout << std::setw(12) << p.x << "\t" <<std::setw(12) << p.y << std::endl;
+  }
 
 
 
