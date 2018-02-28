d55134a8f2ab47829bedd0cb3770a91cd905f622
Felice Pantaleo <felixpantaleo@gmail.com>
2018-02-28 18:42:52 +0100

    fixing energy dist

--- clustering/ticl/dist_generator.h
+++ clustering/ticl/dist_generator.h
@@ -5,7 +5,7 @@
 #include "hgcPoint.h"
 
 
-std::vector<hgcPoint> generate_cluster(float avgX, float avgY, float peakE, float sigmaXY, unsigned int nPoints)
+std::vector<hgcPoint> generate_cluster(int clusterId, float avgX, float avgY, float peakE, float sigmaXY, unsigned int nPoints)
 {
     std::vector<hgcPoint> tmpPoints;
     tmpPoints.reserve(nPoints);
@@ -13,11 +13,77 @@ std::vector<hgcPoint> generate_cluster(float avgX, float avgY, float peakE, floa
     std::normal_distribution<float> x_distribution(avgX, sigmaXY);
     std::normal_distribution<float> y_distribution(avgY, sigmaXY);
     for (auto i=0; i<nPoints; ++i) {
-      tmpPoints.emplace_back(x_distribution(generator), y_distribution(generator), 1) ;
+        auto x = x_distribution(generator);
+        auto y = y_distribution(generator);
+        auto dist = std::sqrt((x- avgX)*(x- avgX) + (y - avgY) * (y - avgY));
+      tmpPoints.emplace_back(x, y, 1000*peakE*std::exp(-2.f*dist), clusterId) ;
+    }
+    return tmpPoints;
+}
 
+
+void generate_cluster(std::vector<hgcPoint>& points, int clusterId, float avgX, float avgY, float peakE, float sigmaXY, unsigned int nPoints)
+{
+    points.reserve(points.size() + nPoints);
+    std::default_random_engine generator;
+    std::normal_distribution<float> x_distribution(avgX, sigmaXY);
+    std::normal_distribution<float> y_distribution(avgY, sigmaXY);
+    for (auto i=0; i<nPoints; ++i) {
+        auto x = x_distribution(generator);
+        auto y = y_distribution(generator);
+        auto dist = std::sqrt((x- avgX)*(x- avgX) + (y - avgY) * (y - avgY));
+      points.emplace_back(x, y, peakE*std::exp(-2.f*dist), clusterId) ;
+    }
+
+}
+
+
+
+std::vector<hgcPoint> generate_noise( float minX, float maxX, float minY, float maxY, unsigned int nPoints)
+{
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> x_distribution(minX,maxX);
+    std::uniform_real_distribution<float> y_distribution(minY,maxY);
+    std::normal_distribution<float> E_distribution(0.5f, 0.3f);
+    std::vector<hgcPoint> tmpPoints;
+    tmpPoints.reserve(nPoints);
+    float tmpE = 0.f;
+    for (auto i=0; i<nPoints; ++i) {
+        do
+        {
+            tmpE = E_distribution(generator);
+
+        } while(tmpE<0.f);
+
+
+
+      tmpPoints.emplace_back(x_distribution(generator), y_distribution(generator), 1, -1) ;
     }
 
     return tmpPoints;
+}
+
+
+void generate_noise(std::vector<hgcPoint>& points, float minX, float maxX, float minY, float maxY, unsigned int nPoints)
+{
+    std::default_random_engine generator;
+    std::uniform_real_distribution<float> x_distribution(minX,maxX);
+    std::uniform_real_distribution<float> y_distribution(minY,maxY);
+    std::normal_distribution<float> E_distribution(0.5f, 0.3f);
+    points.reserve(points.size() + nPoints);
+    float tmpE = 0.f;
+    for (auto i=0; i<nPoints; ++i) {
+        do
+        {
+            tmpE = E_distribution(generator);
+
+        } while(tmpE<0.f);
+
+
+
+      points.emplace_back(x_distribution(generator), y_distribution(generator), 1, -1) ;
+    }
+
 }
 
 
