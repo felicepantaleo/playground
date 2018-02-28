354af12851c7452b401fcf452cd662bb56536a71
Felice Pantaleo <felixpantaleo@gmail.com>
2018-02-28 18:10:32 +0100

    adding simple constructor

--- clustering/ticl/hgcPoint.h
+++ clustering/ticl/hgcPoint.h
@@ -2,11 +2,14 @@
 #define __HGCPOINT_H__
 struct hgcPoint
 {
+    hgcPoint(float a, float b, float c):
+    x(a), y(b), E(c)
+    {
 
+    }
+    float x,y,E;
 
-
-    
-}
+};
 
 
 #endif /* end of include guard: __HGCPOINT_H__ */
