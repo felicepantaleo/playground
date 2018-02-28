82404d9777a0b6ea98c2215fe78e809daae76373
Felice Pantaleo <felixpantaleo@gmail.com>
2018-02-28 17:22:00 +0100

    generating clusters support function

---
+++ clustering/ticl/dist_generator.h
@@ -0,0 +1,20 @@
+#ifndef __DIST_GENERATOR_H_
+#define __DIST_GENERATOR_H_
+
+
+#include <random>
+#include "hgcPoint.h"
+
+std::vector<hgcPoint> generate_cluster(float avgX, float avgY, float peakE, float sigmaXY, unsigned int nPoints)
+{
+    std::vector<hgcPoint> tmpPoints;
+    tmpPoints.reserve(nPoints);
+
+
+
+
+
+}
+
+
+#endif /* end of include guard: __DIST_GENERATOR_H_ */
