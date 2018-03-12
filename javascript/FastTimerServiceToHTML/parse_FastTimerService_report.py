fed28240601f69934bd8749822a19d810056cd01
Felice Pantaleo <felixpantaleo@gmail.com>
2018-03-12 11:28:24 +0100

    input file from command line

--- javascript/FastTimerServiceToHTML/parse_FastTimerService_report.py
+++ javascript/FastTimerServiceToHTML/parse_FastTimerService_report.py
@@ -2,7 +2,9 @@ import  urllib3    # the lib that handles the url stuff
 import pandas as pd
 urllib3.disable_warnings()
 http = urllib3.PoolManager()
-target_url = "https://fpantale.web.cern.ch/fpantale/step3_PU.log"
+target_url = input("URL of the FastTimerService log file: ")
+output_html_file = input("output file: ")
+
 
 r = http.request('GET', target_url)
 text = r.data.decode()
@@ -25,13 +27,13 @@ df.columns = ['Average CPU Time (ms)',
               'Allocated Memory (kB)']
 
 
-file = open('timereport.html','w')
+file = open(output_html_file,'w')
 file.write('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<title>Time Report</title>\n<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.2.1/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/b-html5-1.5.1/b-print-1.5.1/cr-1.4.1/fc-3.2.4/kt-2.3.2/r-2.2.1/rg-1.0.2/sc-1.4.4/sl-1.2.5/datatables.min.css"/>\n<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>\n<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>\n<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.2.1/dt-1.10.16/af-2.2.2/b-1.5.1/b-colvis-1.5.1/b-html5-1.5.1/b-print-1.5.1/cr-1.4.1/fc-3.2.4/kt-2.3.2/r-2.2.1/rg-1.0.2/sc-1.4.4/sl-1.2.5/datatables.min.js"></script>\n<script type="text/javascript" language="javascript" class="init" src="dataTableCustomization.js"></script>\n</head>\n')
 
 
 
 file.close()
-file = open('timereport.html','a')
+file = open(output_html_file,'a')
 file.write('''<button id="button">Row count</button>
 <div id="avgCPUTimeSelTmp"></div>
 <div id="avgCPUTimeOnlySelTmp"></div>
